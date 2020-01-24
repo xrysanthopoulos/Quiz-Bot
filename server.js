d587dd3f3ac4d75d12c4f6da889f6da330f151c9
Dimitris Xrysanthopoulos <dimitris.xrysanthopoulos@gmail.com>
2020-01-24 17:11:07 +0200

    Update server.js

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ async function setupPersistentMenu(res) {
     {
         "persistent_menu": [{
             "locale": "default",
-            "composer_input_disabled": true,
+            "composer_input_disabled": false,
             "call_to_actions": [
                 {
                     "title": 'Νέο παιχνίδι 🎮',
